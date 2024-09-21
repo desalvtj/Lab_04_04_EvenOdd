ce4029b482658df89950d9d89dec58ad39f29ee3
desalvtj <desalvtj@mail.uc.edu>
2024-09-21 11:50:07 -0400

    Complete and Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final int EVEN_TWO = 2;
+        int numToExamine = 7;
+
+
+            if (numToExamine % EVEN_TWO == 0)
+            {
+                System.out.println(numToExamine + " is even");
+            }
+            else
+            {
+            System.out.println(numToExamine + " is odd");
+            }
+
+
+
     }
+
 }
